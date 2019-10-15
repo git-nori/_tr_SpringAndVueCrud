56f587fa884e2e83a4fe29d8b32e8e9b67c7b629
HironoriSato <maza.hs5360@gmail.com>
2019-10-15 21:58:26 +0900

    [add]routerの設定とコンポーネントの新規作成

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
 
@@ -10,16 +9,25 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'customers',
+      component: () => import('./views/Customer.vue'),
+      children: [{
+        path: "/customer/:id",
+        name: "customer-details",
+        component: () => import('./views/Customer.vue'),
+        props: true
+      }
+      ]
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+      path: '/add',
+      name: 'add',
+      component: () => import('./views/AddCustomer.vue')
+    },
+    {
+      path: '/search',
+      name: 'search',
+      component: () => import('./views/SearchCustomers.vue')
+    },
   ]
 })
