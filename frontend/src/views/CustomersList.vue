6bdb9e121372013594a4999f902f1963979b3a62
HironoriSato <maza.hs5360@gmail.com>
2019-10-15 23:43:20 +0900

    [add]CustomersList画面を実装

--- frontend/src/views/CustomersList.vue
+++ frontend/src/views/CustomersList.vue
@@ -1,4 +1,39 @@
-<template></template>
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <h4>Customers List</h4>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-simple-table>
+          <thead>
+            <tr>
+              <th v-for="header in headers" :key="header">{{ header }}</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="customer in customers" :key="customer.id">
+              <td>{{ customer.id }}</td>
+              <td>{{ customer.name }}</td>
+              <td>{{ customer.age }}</td>
+              <td class="text-center">
+                <router-link
+                  :to="{
+                  name:'customer-details',
+                  params: { id: customer.id, customer: customer}
+                  }"
+                >detail</router-link>
+              </td>
+            </tr>
+          </tbody>
+        </v-simple-table>
+      </v-col>
+    </v-row>
+    <router-view></router-view>
+  </v-container>
+</template>
 
 <script>
 import http from "../http-common";
@@ -7,18 +42,26 @@ export default {
   name: "customers-list",
   data() {
     return {
-      customers: []
+      customers: [],
+      headers: []
     };
   },
   methods: {
-    retrieveCustomers() {
-      http.get("/customers").then(res => {
+    async retrieveCustomers() {
+      try {
+        const res = await http.get("/customers");
         this.customers = res.data;
-        console.log(res.data);
-      });
+        this.getHeadersList();
+      } catch (error) {
+        console.log(error);
+      }
     },
-    refreshList() {
-      this.retrieveCustomers();
+    // active以外のcustomersのkeyを取得し、headersに格納する
+    getHeadersList() {
+      const arr = Object.keys(this.customers[0]).filter(
+        val => val !== "active"
+      );
+      this.headers = arr.concat("");
     }
   },
   mounted() {
