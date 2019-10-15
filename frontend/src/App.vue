7e03a39d41934392904d7ad4bddb615d149e8748
HironoriSato <maza.hs5360@gmail.com>
2019-10-15 21:58:50 +0900

    [mod]Home画面にリンクを追加

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,36 +1,30 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
-
     <v-content>
-      <HelloWorld/>
+      <v-container>
+        <v-row>
+          <v-col align="center">
+            <h1>Vue Springboot example</h1>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col align="center">
+            <v-btn outlined color="info" class="mx-2" to="/">Customers</v-btn>
+            <v-btn outlined color="info" class="mx-2" to="/add">Customers</v-btn>
+            <v-btn outlined color="info" class="mx-2" to="/search">Customers</v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+      <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
+  name: "App",
   data: () => ({
     //
-  }),
+  })
 };
 </script>
